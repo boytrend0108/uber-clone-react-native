985dd031fe9b6ebd534cb92c8b7e382b6be7ba9f
boytrend0108 <boytrend@gmail.com>
2025-08-04 09:00:48 +0300

    feat: create onboard page

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const SignIn = () => {
+  return (
+    <SafeAreaView>
+      <Text>Signin</Text>
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
